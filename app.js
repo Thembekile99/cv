cbfc90a0bacd5d3819edf0792106a64cc94151c6
DA MAC M1 100 <damacm1100@DA-Mac100s-MacBook-Air.local>
2023-07-19 15:58:23 +0200

    second changes on my cv

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+function send() {
 
-function myFunction() {
-    document.getElementById("myForm").submit();}
+   let name = document.getElementById("name")
+   let email = document.getElementById("email")
+  
+   let message = document.getElementById("massage")
+
+   let demo = document.getElementById("demo");
+
+    if (name.value == '') {
+       demo.innerHTML = 'Name is required'
+       return
+   }
+
+   if (email.value == '') {
+       demo.innerHTML = 'Email is required'
+       return
+   }
+
+
+  
+   if (message.value == '') {
+       demo.innerHTML = 'Please write your message'
+       return
+   }
+
+
+}
 
